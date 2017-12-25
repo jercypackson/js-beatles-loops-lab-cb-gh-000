12dfbc628aa6a4462cbf5342a1b399fabc1cd98c
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 12:24:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function theBeatlesPlay(musicians, instruments){
   }
   return rv;
 }
+
+function johnLennonFacts(array){
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i] + "!!!";
+  }
+}
