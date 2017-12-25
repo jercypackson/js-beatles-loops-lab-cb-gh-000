44e4d333c988307c6c93347ceb7b62a691b34c78
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 12:22:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function theBeatlesPlay(){
-  
+function theBeatlesPlay(musicians, instruments){
+  var rv = {};
+  for (var i = 0; i < musicians.length; i++) {
+    rv.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return rv;
 }
